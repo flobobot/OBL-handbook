ff5aa27bfda3f404f5cde9be77a12ae35ba20ecf
flobobot <florian.laws@gmail.com>
2025-06-20 06:37:36 +0100

    Use env variable for loading Google Drive credentials

--- netlify/functions/load-cocktail-data.js
+++ netlify/functions/load-cocktail-data.js
@@ -1,37 +1,39 @@
 const { google } = require('googleapis');
-const path = require('path');
-const fs = require('fs');
 
 exports.handler = async function (event, context) {
   if (event.httpMethod !== 'GET') {
     return {
       statusCode: 405,
-      body: 'Method Not Allowed'
+      body: 'Method Not Allowed',
     };
   }
 
   try {
-    const authPath = path.resolve(__dirname, '../../flobocorp-automation-cac461eb14c1.json');
+    // Parse credentials from environment variable
+    const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS_JSON);
 
     const auth = new google.auth.GoogleAuth({
-      keyFile: authPath,
-      scopes: ['https://www.googleapis.com/auth/drive.readonly']
+      credentials,
+      scopes: ['https://www.googleapis.com/auth/drive.readonly'],
     });
 
     const drive = google.drive({ version: 'v3', auth });
 
     // Replace with the actual file ID of your cocktail_data_backup.json
-    const fileId = '1ZB1ddghCaKwh4yNdEU8HRGo3ZV8rHkX9'; // Example ID
+    const fileId = '1ZB1ddghCaKwh4yNdEU8HRGo3ZV8rHkX9';
 
-    const res = await drive.files.get({
-      fileId,
-      alt: 'media'
-    }, { responseType: 'stream' });
+    const res = await drive.files.get(
+      {
+        fileId,
+        alt: 'media',
+      },
+      { responseType: 'stream' }
+    );
 
     let data = '';
     await new Promise((resolve, reject) => {
       res.data
-        .on('data', chunk => data += chunk)
+        .on('data', (chunk) => (data += chunk))
         .on('end', resolve)
         .on('error', reject);
     });
@@ -39,13 +41,12 @@ exports.handler = async function (event, context) {
     return {
       statusCode: 200,
       headers: { 'Content-Type': 'application/json' },
-      body: data
+      body: data,
     };
-
   } catch (err) {
     return {
       statusCode: 500,
-      body: `Failed to load cocktail data: ${err.message}`
+      body: `Failed to load cocktail data: ${err.message}`,
     };
   }
 };
